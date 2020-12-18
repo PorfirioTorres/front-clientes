761fd5c4907cf5f5d54776ea4c4851709a431181
PorfirioTorres <56423905+PorfirioTorres@users.noreply.github.com>
2020-12-18 12:34:40 -0600

    Update cliente.service.ts

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -12,78 +12,35 @@ import { AuthService } from './auth.service';
 @Injectable({
     providedIn: 'root'
 })
-// *** */ El codigo comentado deja de ser util en caso de usan Interceptors ***/
+
 export class ClienteService {
     private urlEndPoint: string;
-   // private httpHeaders: HttpHeaders; // no requerido si se usan Interceptors
 
     constructor(private _httpClient: HttpClient, private _router: Router,
         private authService: AuthService) {
         this.urlEndPoint = 'http://localhost:8080/api/clientes';
-       // this.httpHeaders = new HttpHeaders({'Content-Type': 'application/json'}); // no es requerido si se usan Interceptors
     }
-    /*
-    con este metodo se agrega el header de autorizacion, sin usan Interceptors
-    private agregarAuthorizationHeader() {
-        let token = this.authService.token;
-        if (token != null && token != undefined) {
-            return this.httpHeaders.append('Authorization', 'Bearer ' + token);
-        } else {
-            return this.httpHeaders;
-        }
-    }
-    con este metodo se hace la verificacionde autorizacion desde un Interceptor
-    private isNoAutorizado(e): boolean {
-        if (e.status == 401) {
-            if (this.authService.isAuthenticated()) {
-                this.authService.logOut();
-            }
-          this._router.navigate(['/login']);
-          return true;
-        }
-
-        if (e.status == 403) {
-            swal('Acceso denegado', 'Usted no tiene acceso a este recurso', 'warning');
-            this._router.navigate(['/clientes']);
-            return true;
-          }
-        return false;
-      }*/
 
     getRegiones(): Observable<Region[]> {
-        // return this._httpClient.get<Region[]>(this.urlEndPoint + '/regiones', {headers: this.agregarAuthorizationHeader()}).pipe(
-        return this._httpClient.get<Region[]>(this.urlEndPoint + '/regiones'); /*.pipe(
-
-            catchError(e => {
-                // this.isNoAutorizado(e);
-                return throwError(e);
-            })
-        );*/
+        return this._httpClient.get<Region[]>(this.urlEndPoint + '/regiones');
     }
 
     getClientes(page: number): Observable<any> {
-        // return of(CLIENTES); // con of se convierte a observable, un objeto observable es un stream de datos
-        // los que quieran acceder a ese stream se deben suscribir
        return this._httpClient.get(this.urlEndPoint + '/page/' + page).pipe(
             map((response: any) => response)
        );
     }
 
     create(cliente: Cliente): Observable<any> {
-       // return this._httpClient.post<any>(this.urlEndPoint, cliente, {headers: this.agregarAuthorizationHeader()})
        return this._httpClient.post<any>(this.urlEndPoint, cliente)
        .pipe(
             catchError(e => {
-               /*if (this.isNoAutorizado(e)) {
-                return throwError(e);
-               }*/
                 if (e.status == 400) {
                     return throwError(e);
                 } else {
                     if (e.error.mensaje) {
                         console.log(e.error.mensaje);
                     }
-                   // swal (e.error.mensaje, e.error.error, 'error');
                     return throwError(e);
                 }
             })
@@ -91,30 +48,21 @@ export class ClienteService {
     }
 
     getCliente(id: number): Observable<Cliente> {
-       // return this._httpClient.get<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.agregarAuthorizationHeader()}).pipe(
         return this._httpClient.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
         catchError(e => {
-                /*if (this.isNoAutorizado(e)) {
-                    return throwError(e);
-                }*/
                 if (e.status != 401 && e.error.mensaje) {
                     console.log(e.error.mensaje);
                     this._router.navigate(['/clientes']);
                 }
-                // swal('Error al buscar', e.error.mensaje, 'error');
                 return throwError(e);
             })
         );
     }
 
     update(cliente: Cliente): Observable<any> {
-       // return this._httpClient.put<any>(this.urlEndPoint, cliente, {headers: this.agregarAuthorizationHeader()})
        return this._httpClient.put<any>(this.urlEndPoint, cliente)
        .pipe(
             catchError(e => {
-                /*if (this.isNoAutorizado(e)) {
-                    return throwError(e);
-                }*/
                 if (e.status == 400) {
                     return throwError(e);
                 } else {
@@ -129,13 +77,9 @@ export class ClienteService {
     }
 
     delete(id: number): Observable<Cliente> {
-       // return this._httpClient.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.agregarAuthorizationHeader()})
        return this._httpClient.delete<Cliente>(`${this.urlEndPoint}/${id}`)
        .pipe(
             catchError(e => {
-                /*if (this.isNoAutorizado(e)) {
-                    return throwError(e);
-                }*/
                 if (e.error.mensaje) {
                     console.log(e.error.mensaje);
                 }
@@ -147,17 +91,12 @@ export class ClienteService {
 
     subirFoto(foto: File, id: any): Observable<HttpEvent<{}>> {
         let fdata = new FormData();
-        fdata.append('imgFile', foto); // el mismo nombre que definimos en el RequestParam
+        fdata.append('imgFile', foto);
         fdata.append('id', id);
 
         const req = new HttpRequest('POST', `${this.urlEndPoint}/upload`, fdata, {
             reportProgress: true
           });
-        return this._httpClient.request(req); /*.pipe(
-            catchError(e => {
-                this.isNoAutorizado(e);
-                return throwError(e);
-            })
-        );*/
+        return this._httpClient.request(req); 
     }
 }
